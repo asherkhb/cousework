cb7c21aa651724ef21817c7e29a940b8c68d7a9e
asherkhb <asherkhb@gmail.com>
2015-10-02 11:27:57 -0700

    functional, serialized script

--- INFO520/fits_head_extract.py
+++ INFO520/fits_head_extract.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 
 import argparse
-from os import listdir
+from os import listdir, path
 
 __author__ = 'asherkhb'
+
+def extract_metadata(fits_file):
+    f = pyfits.open(fits_file)
+    filename = path.basename(fits_file)
+    head = f[0].header
+    # print repr(head)  # Print entire header
+    obsdate = head['DATE-OBS']
+    vimtype = head['VIMTYPE']
+    vshutter = head['VSHUTTER']
+
+    metadata = {"filename": filename,
+                "obsdate": obsdate,
+                "vimtype": vimtype,
+                "vshutter": vshutter}
+
+    f.close()
+    return metadata
+
+
+def print_summary(img_total, science_count, dark_count, open_count, shut_count, error_count):
+    print "Dataset Composition\n" \
+      "> TOTAL Images: %d\n" \
+      "> SCIENCE Images: %d\n" \
+      "> DARK Images: %d\n" \
+      "> OPEN Shutter: %s\n" \
+      "> CLOSED Shutter: %d\n" \
+      "> ERRORS: %d" % (img_total, science_count, dark_count, open_count, shut_count, error_count)
+
 
 # Future options:
 #    Inputs
@@ -19,18 +47,14 @@ parser.add_argument('-p',
                     type=str,
                     default="./",
                     help="path to images")
-
 parser.add_argument('-o',
                     type=str,
                     default="output.csv",
                     help="output metadata CSV filename")
-
 parser.add_argument("-H",
                     action="store_true",
                     help="use HPC mode")
-
 args = parser.parse_args()
-#print args
 
 # Import appropriate FITS library.
 if args.H:
@@ -51,95 +75,47 @@ for f in listdir(img_directory):
     else:
         alt_files.append(f)
 
-# Summary Variables
+# Summary variables.
 science_count = 0
 dark_count = 0
 open_count = 0
 shut_count = 0
 error_count = 0
 
-
 # Analyze each file.
 csv_entries = []
 for fits_file in file_queue:
     file_location = img_directory + '/' + fits_file
-    current_file = pyfits.open(file_location)
+    meta = extract_metadata(file_location)
 
-    # Print HDUList info
-    #current_file.info()
-
-    # Create Header Object
-    head = current_file[0].header
-
-    # Print entire header
-    #print repr(head)
-
-    # Get Observation Date
-    # DATE-OBS= '2014-11-04T05:35:16.481449' / Date of obs. YYYY-mm-ddTHH:MM:SS
-    obsdate = head['DATE-OBS']
-    #obsdate = obsdate.replace('-', '').replace('T', '').replace(':', '').replace('.', '')
-    #print obsdate
-
-    # Get Image Type
-    # VIMTYPE = 'SCIENCE '           / Image Type
-    vimtype = head['VIMTYPE']
-    #print vimtype
-
-    # Get Shutter State
-    # VSHUTTER= 'OPEN    '           / Status of VisAO Shutter
-    vshutter = head['VSHUTTER']
-    #print vshutter
-
-    # Check for Completeness (Could be replaced with GREP)
-    if vimtype == "SCIENCE":
+    # Populate summary variables.
+    if meta["vimtype"] == 'SCIENCE':
         science_count += 1
-        if vshutter == "OPEN":
-            open_count += 1
-            pass
-        else:
-            if vshutter == "SHUT":
-                shut_count += 1
-            error_count += 1
-            print "ERROR1: Type/shutter mismatch [%s, %s] - %s" % (vimtype, vshutter, fits_file)
-
-    elif vimtype == "DARK":
+    elif meta["vimtype"] == "DARK":
         dark_count += 1
-        if vshutter == "SHUT":
-            shut_count += 1
-        else:
-            if vshutter == "OPEN":
-                open_count += 1
-            error_count += 1
-            print "ERROR2: Type/shutter mismatch [%s, %s] - %s" % (vimtype, vshutter, fits_file)
-
     else:
-        if vshutter == "OPEN":
-            open_count += 1
-        elif vshutter == "SHUT":
-            shut_count += 1
         error_count += 1
-        print "ERROR3: Unknown vimtype [%s] - %s" % (vimtype, fits_file)
+        print "ERROR: Unrecognized Image Type (%s)" % meta["vimtype"]
+
+    if meta["vshutter"] == 'OPEN':
+        open_count += 1
+    elif meta["vshutter"] == 'SHUT':
+        shut_count += 1
+    else:
+        error_count += 1
+        print "ERROR: Unrecognized Shutter State (%s)" % meta["shutter"]
 
     # Compile Entry and append to entries list
-    file_entry = "%s,%s,%s,%s\n" % (fits_file.rstrip(".fits"), obsdate, vimtype, vshutter)
+    file_entry = "%s,%s,%s,%s\n" % (meta["filename"], meta["obsdate"], meta["vimtype"], meta["vshutter"])
     csv_entries.append(file_entry)
 
-    # Close file
-    current_file.close()
-
-# Generate output CSV
+# Generate output CSV.
 csv_header = "FILENAME,DATETIME,IMAGETYPE,SHUTTER\n"
 with open(output_file, 'w') as otpt:
     otpt.write(csv_header)
     for entry in csv_entries:
         otpt.write(entry)
 
-# Write to stdout summary statistics.
+# Write summary to stdout.
 print "FITS-HEAD-EXTRACT is completed!"
-print "Dataset Composition\n" \
-      "> TOTAL Images: %d\n" \
-      "> SCIENCE Images: %d\n" \
-      "> DARK Images: %d\n" \
-      "> OPEN Shutter: %s\n" \
-      "> CLOSED Shutter: %d\n" \
-      "> ERRORS: %d" % (len(file_queue), science_count, dark_count, open_count, shut_count, error_count)
+print_summary(len(file_queue), science_count, dark_count, open_count, shut_count, error_count)
